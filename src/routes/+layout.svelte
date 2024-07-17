7bac542350cba526d535e974940f05c644a0136c
Motocarota <motocarota@gmail.com>
2024-07-17 17:35:02 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+	import "../app.css";
+</script>
+
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a class="btn btn-ghost text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none">
+		<button class="btn btn-square btn-ghost">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				class="inline-block h-5 w-5 stroke-current"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+				></path>
+			</svg>
+		</button>
+	</div>
+</div>
+
+<slot />
+
+<footer class="footer bg-neutral text-neutral-content p-10">
+	<nav>
+		<h6 class="footer-title">Services</h6>
+		<a class="link link-hover">Branding</a>
+		<a class="link link-hover">Design</a>
+		<a class="link link-hover">Marketing</a>
+		<a class="link link-hover">Advertisement</a>
+	</nav>
+	<nav>
+		<h6 class="footer-title">Company</h6>
+		<a class="link link-hover">About us</a>
+		<a class="link link-hover">Contact</a>
+		<a class="link link-hover">Jobs</a>
+		<a class="link link-hover">Press kit</a>
+	</nav>
+	<nav>
+		<h6 class="footer-title">Legal</h6>
+		<a class="link link-hover">Terms of use</a>
+		<a class="link link-hover">Privacy policy</a>
+		<a class="link link-hover">Cookie policy</a>
+	</nav>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  let cards = [{ name: "ciao" }, { name: "miao" }, { name: "bao" }];
+</script>
+
+<div
+  class="hero min-h-screen"
+  style="background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);"
+>
+  <div class="hero-overlay bg-opacity-60"></div>
+  <div class="hero-content text-neutral-content text-center">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <p class="mb-5">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+
+<div class="min-h-screen flex flex-wrap gap-1 p-11 mx-auto max-w-screen-lg">
+  {#each cards as c}
+    <div class="card bg-base-100 w-96 shadow-xl h-96">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+          alt="car!"
+        />
+      </figure>
+      <div class="card-body">
+        <h2 class="card-title">{c.name}</h2>
+        <p>How to park your car at your garage?</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-primary">Learn now!</button>
+        </div>
+      </div>
+    </div>
+  {/each}
+</div>
