67760625abd942f09d5faa45839bb49fff081eb8
Motocarota <motocarota@gmail.com>
2024-07-17 17:51:22 +0200

    adds support to gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: true,
+			fallback: undefined,
+			// fallback: '404.html',
+		}),
+		paths: {
+			assets: '',
+			base: '',
+			relative: true,
+		},
+	}
 };
 
 export default config;
