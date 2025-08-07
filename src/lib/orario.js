1121cb087b3d150c4feb8261d4f3bdc4fd020453
Motocarota <motocarota@gmail.com>
2025-08-07 15:16:28 +0200

    fix orario

--- src/lib/orario.js
+++ src/lib/orario.js
@@ -5,26 +5,26 @@ export const orario = [
   },
   {
     giorno: 'Martedì',
-    orario: '19:00 - 22:30',
+    orario: '19:00 - 22:00',
   },
   {
     giorno: 'Mercoledì',
-    orario: '19:00 - 22:30',
+    orario: '19:00 - 22:00',
   },
   {
     giorno: 'Giovedì',
-    orario: '19:00 - 22:30',
+    orario: '19:00 - 22:00',
   },
   {
     giorno: 'Venerdì',
-    orario: '19:00 - 22:30',
+    orario: '19:00 - 22:00',
   },
   {
     giorno: 'Sabato',
-    orario: '19:00 - 22:30',
+    orario: '19:00 - 22:00',
   },
   {
     giorno: 'Domenica',
-    orario: '19:00 - 22:30',
+    orario: '19:00 - 22:00',
   },
 ];
